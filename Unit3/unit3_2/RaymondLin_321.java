febefc479f953dd3b75c9e6bb11f0d36e507146f
linra <linra@214B-008.plasma.utschools.ca>
2019-01-07 15:53:19 -0500

    3.2 started

---
+++ Unit3/unit3_2/RaymondLin_321.java
@@ -0,0 +1,37 @@
+package unit3_2;
+
+public class RaymondLin_321 
+{
+	public static void main(String[] args) 
+	{
+		final long startTime = System.nanoTime();
+		System.out.println(power(1.0005,5000));
+		final long endTime = System.nanoTime();
+		System.out.println("Total execution time: " + (endTime - startTime));
+	}
+	public static double power(double x, long n)
+	{
+		if(n==1) return x;
+		double a = power(x,n/2);
+		if(n%2 == 0)
+		{
+			return a*a;
+		}
+		return x*a*a;
+	}
+
+	public static double power1(double x, long n)
+	{
+		if(n==1)
+			return x;
+		if(n%2 == 0)
+		{
+			return power1(x,n/2)*power1(x,n/2);
+		}
+		else
+		{
+			return x*power1(x,n-1);
+		}
+			
+	}
+}
---
+++ Unit3/unit3_2/RaymondLin_322.java
@@ -0,0 +1,15 @@
+package unit3_2;
+
+public class RaymondLin_322
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(reverseString("abcdefg"));
+	}
+	public static String reverseString(String word)
+	{
+		if(word.length()<2)
+			return word;
+		return reverseString(word.substring(1))+word.charAt(0);
+	}
+}
